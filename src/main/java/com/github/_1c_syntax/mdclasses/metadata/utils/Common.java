d1b9a98f72a51557a5fe847f5b57ab99a77f5ae7
Tymko Oleg <olegtymko@yandex.ru>
2019-11-16 19:22:03 +0700

    проба прокинуть состояние модулей в конфигурацию от конфигуратора

--- src/main/java/com/github/_1c_syntax/mdclasses/metadata/utils/Common.java
+++ src/main/java/com/github/_1c_syntax/mdclasses/metadata/utils/Common.java
@@ -1,17 +1,24 @@
 package com.github._1c_syntax.mdclasses.metadata.utils;
 
+import com.github._1c_syntax.mdclasses.metadata.SupportDataConfiguration;
+import com.github._1c_syntax.mdclasses.metadata.additional.ModuleType;
+import com.github._1c_syntax.mdclasses.metadata.additional.SupportVariant;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import com.github._1c_syntax.mdclasses.metadata.additional.ModuleType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Common {
@@ -20,6 +27,10 @@ public class Common {
 
   public static final String EXTENSION_BSL = "bsl";
   public static final String FILE_SEPARATOR = Pattern.quote(System.getProperty("file.separator"));
+
+  public static final String REGEX_GUID = "uuid=\"(.*)\"";
+  public static final Pattern PATTERN_REGEX_GUID = Pattern.compile(REGEX_GUID, Pattern.MULTILINE);
+  public static final String EXTENSION_XML = "xml";
 
   public static ModuleType changeModuleTypeByFileName(String fileName, String secondFileName) {
 
@@ -74,6 +85,126 @@ public class Common {
     });
 
     return modulesByType;
+
+  }
+
+  public static Map<URI, SupportVariant> getModuleSupportByPath(Path rootPath, Map<URI, ModuleType> modulesByType) {
+
+    Map<URI, SupportVariant> modulesBySupport = new HashMap<>();
+
+    // TODO: для EDT по другому
+    File fileParentConfiguration = new File(rootPath.toString(), "Ext/ParentConfigurations.bin");
+
+    if (fileParentConfiguration.exists()) {
+      SupportDataConfiguration supportDataConfiguration = new SupportDataConfiguration(fileParentConfiguration.toPath());
+      final Map<String, SupportVariant> supportMap = supportDataConfiguration.getSupportMap();
+
+      String rootPathString = rootPath.toString() + System.getProperty("file.separator");
+      Collection<File> files = FileUtils.listFiles(rootPath.toFile(), new String[]{EXTENSION_BSL}, true);
+
+      files.parallelStream().forEach(file -> {
+        URI uri = file.toURI();
+        String[] elementsPath =
+            file.toPath().toString().replace(rootPathString, "").split(FILE_SEPARATOR);
+
+        SupportVariant moduleSupport = null;
+        String objectGuid = getObjectGuid(rootPath, uri, elementsPath, modulesByType.get(uri));
+        if (!objectGuid.isEmpty()) {
+          moduleSupport = supportMap.get(objectGuid);
+        }
+
+        if (moduleSupport == null) {
+          moduleSupport = SupportVariant.NONE;
+        }
+
+        modulesBySupport.put(uri, moduleSupport);
+
+      });
+    }
+    return modulesBySupport;
+  }
+
+
+  private static String getObjectGuid(Path rootPath, URI uri, String[] elementsPath, ModuleType moduleType) {
+    String guid = "";
+    String path = "";
+    if (moduleType == ModuleType.ApplicationModule
+        || moduleType == ModuleType.ExternalConnectionModule
+        || moduleType == ModuleType.ManagedApplicationModule
+        || moduleType == ModuleType.OrdinaryApplicationModule
+        || moduleType == ModuleType.SessionModule) {
+      path = new File(rootPath.toString(), "Configuration.xml").toPath().toString();
+    } else {
+      String currentElement = elementsPath[elementsPath.length - 2];
+      if (currentElement.equalsIgnoreCase("Ext")) {
+        String second = elementsPath[elementsPath.length - 4];
+        if (second.equalsIgnoreCase("Commands")) {
+          path = getSimplePath(rootPath, elementsPath, 5);
+        } else {
+          path = getSimplePath(rootPath, elementsPath, 3);
+        }
+      } else if (currentElement.equalsIgnoreCase("Form")) {
+        path = getSimplePath(rootPath, elementsPath, 4);
+      } else {
+        LOGGER.info("Не найден идентификатор файла: " + uri.toString());
+      }
+    }
+
+    guid = getGuidByFile(path);
+    return guid;
+  }
+
+  public static String getGuidByFile(String path) {
+    File xml = new File(path);
+    String guid = "";
+    if (xml.exists()) {
+      guid = findGuidIntoFileXML(xml);
+    }
+    return guid;
+  }
+
+  public static String getSimplePath(Path rootPath, String[] elementsPath, int startPosition) {
+    String path = "";
+    int position = 0;
+    for (String el : elementsPath) {
+      if (position == elementsPath.length - startPosition + 1) {
+        break;
+      }
+      path += el + System.getProperty("file.separator");
+      position++;
+    }
+    path = path.substring(0, path.length() - 1) + "." + EXTENSION_XML;
+    return new File(rootPath.toString(), path).toPath().toString();
+  }
+
+  public static String findGuidIntoFileXML(File file) {
+    String result = "";
+    String content = getContentFile(file);
+    Matcher matcher = PATTERN_REGEX_GUID.matcher(content);
+    if (matcher.find()) {
+      result = matcher.group(1);
+    }
+    return result;
+  }
+
+  public static String getContentFile(File file) {
+    String content = "";
+    try {
+      content = readFile(file.toPath());
+    } catch (Exception e) {
+
+    }
+    return content;
+  }
+
+  public static String readFile(Path path) {
+    String result = "";
+    try {
+      result = new String(Files.readAllBytes(Paths.get(path.toUri())), StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      //LOGGER.error("Don't read bin file", e);
+    }
+    return result;
 
   }
 
